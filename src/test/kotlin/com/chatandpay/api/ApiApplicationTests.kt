cfb313700fc7a2f365c8ca99a654f6173b5cf763
beeniyxz <beeniyxz@gmail.com>
2023-05-02 02:39:35 +0900

    [init] 최초 프로젝트 세팅

---
+++ src/test/kotlin/com/chatandpay/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chatandpay.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
