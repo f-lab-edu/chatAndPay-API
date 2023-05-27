7c97b0be498fdb239eb85af2b83f267f7fc8224b
beeniyxz <beeniyxz@gmail.com>
2023-05-28 02:04:48 +0900

    [feat] #13 오류 코드 반환 Enum 처리

---
+++ src/main/kotlin/com/chatandpay/api/code/BankRspCode.kt
@@ -0,0 +1,20 @@
+package com.chatandpay.api.code
+
+enum class BankRspCode(
+    private val content: String,
+    private val code: String,
+    private val systemUseYn: Boolean
+) {
+
+    RSP000("정상", "000", true),
+    RSP111("출금(개설)기관 SYSTEM 장애", "111", false),
+    RSP114("출금(개설)기관 서비스 시간 아님", "114", false),
+    RSP400("입금 처리 중", "400", false),
+    RSP452("비밀번호 입력횟수 초과", "452", false),
+    RSP453("예금잔액 부족", "453", false),
+    RSP454("출금가능잔액 부족", "454", false),
+    RSP999("순이체한도 초과", "999", false);
+
+    val msg: String = content
+    val bankRspCode: String = code
+}
