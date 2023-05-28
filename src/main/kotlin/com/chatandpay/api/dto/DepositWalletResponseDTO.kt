870c164660234869d24e9bda77447c7c6781ddc8
beeniyxz <beeniyxz@gmail.com>
2023-05-29 01:01:25 +0900

    [feat] #14 Account 오류 반환 공통화 작업

--- src/main/kotlin/com/chatandpay/api/dto/DepositWalletResponseDTO.kt
+++ src/main/kotlin/com/chatandpay/api/dto/DepositWalletResponseDTO.kt
@@ -1,11 +1,11 @@
 package com.chatandpay.api.dto
 
+import com.chatandpay.api.common.ApiResponse
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 
-
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
-data class DepositWalletDTO (
-    val depositMoney: Int,
-    val accountId: Long,
-)
+data class DepositWalletResponseDTO (
+    val chargeAmount : Int,
+    val walletAmount : Int
+) : ApiResponse()
