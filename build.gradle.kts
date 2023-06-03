da123b4cc783c687c5a5a023151fdd07b1f39d9e
beeniyxz <beeniyxz@gmail.com>
2023-06-04 01:13:48 +0900

    [test] login / register 테스트

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation("org.mockito:mockito-inline")
 }
 
 tasks.withType<KotlinCompile> {
