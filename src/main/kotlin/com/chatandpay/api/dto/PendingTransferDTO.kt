0382cb709e3c84e1afa776fe5c6a9f0854f2a9ce
beeniyxz <beeniyxz@gmail.com>
2023-06-19 23:09:42 +0900

    [fix] class to data class

--- src/main/kotlin/com/chatandpay/api/dto/PendingTransferDTO.kt
+++ src/main/kotlin/com/chatandpay/api/dto/PendingTransferDTO.kt
@@ -2,11 +2,11 @@ package com.chatandpay.api.dto
 
 import java.util.*
 
-class PendingTransferDTO (
+data class PendingTransferDTO (
 
     val uuid: UUID,
     val sender: Long,
     val receiver: Long,
     val amount: Int
 
-){}
+)
