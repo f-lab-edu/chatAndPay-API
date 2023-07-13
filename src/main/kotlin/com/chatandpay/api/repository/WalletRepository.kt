ed476870673d9698c067e839ff1e34ef2ea88b65
beeniyxz <beeniyxz@gmail.com>
2023-07-14 01:16:52 +0900

    [feat] Redisson Lock to Wallet

--- src/main/kotlin/com/chatandpay/api/repository/WalletRepository.kt
+++ src/main/kotlin/com/chatandpay/api/repository/WalletRepository.kt
@@ -3,7 +3,6 @@ package com.chatandpay.api.repository
 import com.chatandpay.api.domain.Wallet
 import org.springframework.stereotype.Repository
 import javax.persistence.EntityManager
-import javax.persistence.OptimisticLockException
 import javax.persistence.PersistenceContext
 @Repository
 class WalletRepository {
@@ -12,21 +11,8 @@ class WalletRepository {
     private lateinit var entityManager: EntityManager
 
     fun save(wallet: Wallet): Wallet? {
-        val maxRetries : Long = 5
-        var retryCount = 0
-
-        while (retryCount < maxRetries) {
-            try {
-                entityManager.merge(wallet)
-                entityManager.flush()
-                return wallet
-            } catch (e: OptimisticLockException) {
-                retryCount++
-                Thread.sleep(1000 * maxRetries)
-            }
-        }
-
-        throw OptimisticLockException("최대 재시도 횟수 $maxRetries 번을 초과하였습니다.")
+        entityManager.persist(wallet)
+        return wallet
     }
 
     fun findByPayUserId(payUserId: Long): Wallet? {
