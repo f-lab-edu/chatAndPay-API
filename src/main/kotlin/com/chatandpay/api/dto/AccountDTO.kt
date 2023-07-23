e661be37911c93b0bfd7bbeedc0e0249df07a35b
beeniyxz <beeniyxz@gmail.com>
2023-07-23 23:21:09 +0900

    [feat] AccountDTO Validation

--- src/main/kotlin/com/chatandpay/api/dto/AccountDTO.kt
+++ src/main/kotlin/com/chatandpay/api/dto/AccountDTO.kt
@@ -3,14 +3,22 @@ package com.chatandpay.api.dto
 import com.chatandpay.api.common.ApiResponse
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import javax.validation.constraints.Min
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
 class AccountDTO {
 
     @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
     data class OtherBankAccountRequestDTO(
+        @field:Size(min = 3, max = 3, message = "은행 코드는 3자리의 숫자입니다.")
         val bankCode: String,
+        @field:Pattern(regexp = "^[1-9]*\$", message =  "계좌는 숫자만 입력할 수 있습니다.")
+        @field:Size(min = 8, max = 20, message = "계좌번호는 8~20자 사이입니다.")
         val accountNumber: String,
+        @field:Size(min = 8, max = 20, message = "계좌별칭은 1자 이상 입력해야 합니다.")
         val accountName: String,
+        @field:Size(min = 1, max = 1, message = "Input Error: autoDebitAgree - Y/N")
         val autoDebitAgree: String,
         val userId: Long,
     )
@@ -27,6 +35,7 @@ class AccountDTO {
 
     @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
     data class DepositWalletRequestDTO (
+        @field:Min(value = 1, message = "1원보다 큰 금액만 충전할 수 있습니다.")
         val depositMoney: Int,
         val accountId: Long,
     )
