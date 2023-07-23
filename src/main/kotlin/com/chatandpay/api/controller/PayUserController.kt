88aaf1633de88b4ca42390aa7bdef38539500e78
beeniyxz <beeniyxz@gmail.com>
2023-07-23 16:53:02 +0900

    [feat] PayUser Validation
    [feat] PayUser Validation

--- src/main/kotlin/com/chatandpay/api/controller/PayUserController.kt
+++ src/main/kotlin/com/chatandpay/api/controller/PayUserController.kt
@@ -9,6 +9,7 @@ import com.chatandpay.api.exception.RestApiException
 import com.chatandpay.api.service.PayUserService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 
 @RestController
@@ -17,7 +18,7 @@ class PayUserController(val payUserService: PayUserService)  {
 
 
     @PostMapping("/signup")
-    fun signUpPayUser(@RequestBody payUser: PayUserDTO.SignUpRequestPayUserDTO): ResponseEntity<PayUserDTO.SignUpResponsePayUserDTO> {
+    fun signUpPayUser(@RequestBody @Valid payUser: PayUserDTO.SignUpRequestPayUserDTO): ResponseEntity<PayUserDTO.SignUpResponsePayUserDTO> {
 
         val response = payUserService.register(payUser) ?: throw RestApiException("페이 회원 가입 실패")
         return ResponseEntity.ok(response)
--- src/main/kotlin/com/chatandpay/api/dto/PayUserDTO.kt
+++ src/main/kotlin/com/chatandpay/api/dto/PayUserDTO.kt
@@ -3,6 +3,7 @@ package com.chatandpay.api.dto
 import com.chatandpay.api.common.ApiResponse
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import javax.validation.constraints.Pattern
 
 
 class PayUserDTO {
@@ -12,6 +13,7 @@ class PayUserDTO {
 
         val ci: String,
         val userId: Long,
+       @field:Pattern(regexp = "^(19|20)\\d{2}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$", message = "유효한 생년월일 형식이 아닙니다.")
         val birthDate: String,
 
     )
